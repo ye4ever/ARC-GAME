e05e49ef881e324df0fbd7c391fca5e0934e26cc
ye4ever <ye4ever.com@gmail.com>
2023-01-14 20:00:14 +0100

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "dyxe34",
   e2e: {
-    baseUrl: "http://localhost:3000"
+    baseUrl: "https://arcgame.netlify.app/"
   },
 });
